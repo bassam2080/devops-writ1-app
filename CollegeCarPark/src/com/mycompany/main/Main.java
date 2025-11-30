7dde25570a5d12830b4ce4b831eb76af0b6571c4
SDMatthe <maffshilton@gmail.com>
2025-11-30 17:41:46 +0000

    fixed error where csv file couldn't be found

--- CollegeCarPark/src/com/mycompany/main/Main.java
+++ CollegeCarPark/src/com/mycompany/main/Main.java
@@ -2,13 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-// All imports
-
 package com.mycompany.main;
 import java.util.Scanner;
-
 import static java.lang.System.exit;
-
 
 /**
  *
@@ -16,31 +12,33 @@ import static java.lang.System.exit;
  */
 public class Main {
     public static String filePath = "CollegeCarPark\\new vrn.csv"; //File path to csv file
+    public static Scanner scanner; // Create a class-level scanner
 
     public static void main(String[] args) {
-        menu(); //Call the main menu method
+        scanner = new Scanner(System.in); // Initialize once
+        menu();
+        scanner.close(); // Close when done
     }
 
     public static void printMenu(String[] options){
-
         for (String option : options){
             System.out.println(option);
         }
         System.out.print("Choose your option : ");
     }
+
     public static void menu() {
         String[] options = {
                 "[1] Admin Mode:",
                 "[2] Record Mode:",
                 "[3] Exit",
         };
-        Scanner scanner = new Scanner(System.in); //Get user input for main menu
         int option = 1;
-        while (option!=3){
+        while (option != 3){
             printMenu(options);
             try {
                 option = scanner.nextInt();
-                switch (option){  //Based off user selection will either trigger admin menu or record menu
+                switch (option){
                     case 1: adminMenu(); break;
                     case 2: recordMenu(); break;
                     case 3: exit(0);
@@ -48,42 +46,41 @@ public class Main {
             }
             catch (Exception ex){
                 System.out.println("Please enter an integer value between 1 and " + options.length);
-                scanner.next();
+                scanner.nextLine(); // Clear the entire line including newline
             }
         }
     }
 
     public static void recordMenu() {
-        Scanner userInput = new Scanner(System.in); // Create new scanner object
-        System.out.println("Please enter your VRN: "); //Prompt user to enter their VRN
-        String selection = userInput.nextLine();
-
+        System.out.println("Please enter your VRN: ");
+        String selection = scanner.nextLine(); // Use the class-level scanner
         CarParkRecord myCarParkRecord = new CarParkRecord(filePath);
         myCarParkRecord.parseRecord(selection);
-
     }
+
     public static void adminMenu() {
-        Scanner userInput = new Scanner(System.in); //Get user input for the admin menu
         System.out.println("Please select an option: ");
         System.out.println("[1]Show all vehicles: ");
         System.out.println("[2]Search for VRN: ");
         System.out.println("[3]Amend data: ");
         System.out.println("[4]Delete data");
-        String selection = userInput.nextLine();
-        
+
+        scanner.nextLine(); // Clear any leftover input from previous nextInt()
+        String selection = scanner.nextLine(); // Use the class-level scanner
+
         switch(selection) {
             case "1":
-                CarParkAdmin.displayData(filePath); //Run displayData method
+                CarParkAdmin.displayData(filePath);
                 break;
             case "2":
-                CarParkAdmin.searchVRN(filePath); //Run searchVRN method
+                CarParkAdmin.searchVRN(filePath);
                 break;
-           case "3":
-                CarParkAdmin.amendLine(filePath);// Run amend method
-               break;
+            case "3":
+                CarParkAdmin.amendLine(filePath);
+                break;
             case "4":
-                CarParkAdmin.deleteLine(filePath); // Run delete method
-
+                CarParkAdmin.deleteLine(filePath);
+                break;
         }
     }
 }
